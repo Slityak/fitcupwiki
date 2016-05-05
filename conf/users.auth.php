81716601f352e4378fd8fd5074620ee30940653f
Kornel Kovacs <kornel.kovacs@draftgate.com>
2016-05-05 19:54:16 +0200

    init

---
+++ conf/users.auth.php
@@ -0,0 +1,11 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+kornel:827ccb0eea8a706c4c34a16891f84e7b:Kornél:kornel.kovacs@draftgate.com:user
